1a4b7709902e0f3a570be65190cd100cffddfd04
AustinAdams11 <austinadams011@gmail.com>
2025-07-07 10:28:35 -0400

    test

--- html-stuff/index.js
+++ html-stuff/index.js
@@ -1,3 +1,15 @@
-const message = 'Hello World!';
+const myFunction = (number) => {
+  const functionArray = [
+    (num) => (num += 1),
+    (num) => (num *= 2),
+    (num) => (num *= 534),
+    (num) => (num /= 1),
+    (num) => (math.round()),
+    // add function here...
+  ];
 
-console.log(message);  
+  for (const number of functionArray) {
+      let number = 0
+    console.log functionArray
+  }
+}
